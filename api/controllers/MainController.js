96faee46202f3d394b108d1811600bd5a64b779d
John Oliva <joliva63@gmail.com>
2013-08-14 12:40:08 -0400

    Render /index with default layout and main/index.ejs view file.

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -5,13 +5,17 @@
  * @description	:: Contains logic for handling requests.
  */
 
-module.exports = {
-
-  /* e.g.
-  sayHello: function (req, res) {
-    res.send('hello world!');
-  }
-  */
-  
-
+var MainController = {
+	index: function(req, res) {
+		res.view();
+	},
+	signup: function(req, res) {
+	},
+	login: function(req, res) {
+	},
+	chat: function(req, res) {
+	}
 };
+
+module.exports = MainController;
+
