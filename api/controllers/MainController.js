e4cdeb52ce9138fa073eb0acec2d8cd71a36d614
John Oliva <joliva63@gmail.com>
2013-08-15 18:21:05 -0400

    Added route handlers to the Main controller for signup, login and chat.

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -9,11 +9,68 @@ var MainController = {
 	index: function(req, res) {
 		res.view();
 	},
-	signup: function(req, res) {
+
+	signup: function (req, res) {
+			var username = req.param("username");
+			var password = req.param("password");
+			 
+			Users.findByUsername(username).done(function(err, usr) {
+				if (err) {
+					res.set({ error: "DB Error" });
+					res.send(500);
+				} else if (usr.length > 0) {
+					res.set({error: "Username already Taken"});
+					res.send(400);
+				} else {
+					var hasher = require("password-hash");
+					password = hasher.generate(password);
+					 
+					Users.create({username: username, password: password}).done(function(error, user) {
+					if (error) {
+						res.set({error: "DB Error"});
+						res.send(500);
+					} else {
+						req.session.user = user;
+						res.send(user);
+					}
+				});
+			}
+		});
 	},
-	login: function(req, res) {
+
+	login: function (req, res) {
+		var username = req.param("username");
+		var password = req.param("password");
+		 
+		Users.findByUsername(username).done(function(err, usr) {
+			if (err) {
+				res.set({ error: "DB Error" });
+				res.send(500);
+			} else {
+				if (usr.length > 0) {
+					var user = usr[0];
+					var hasher = require("password-hash");
+					if (hasher.verify(password, user.password)) {
+						req.session.user = user;
+						res.send(user);
+					} else {
+						res.set({ error: "Wrong Password" });
+						res.send(400);
+					}
+				} else {
+					res.set({ error: "User not Found" });
+					res.send(404);
+				}
+			}
+		});
 	},
-	chat: function(req, res) {
+
+	chat: function (req, res) {
+		if (req.session.user) {
+			res.view({username: req.session.user.username});
+		} else {
+			res.redirect('/');
+		}
 	}
 };
 
