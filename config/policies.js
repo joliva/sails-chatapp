9c7aaec49069049164d688e1982124c309ae43c5
John Oliva <joliva63@gmail.com>
2013-08-21 07:08:56 -0400

    Added policy MessageCreatePolicy governing creation of new messages.

--- config/policies.js
+++ config/policies.js
@@ -12,29 +12,31 @@
 
 module.exports.policies = {
 
-  // Default policy for all controllers and actions
-  // (`true` allows public access) 
-  '*': true
+	// Default policy for all controllers and actions
+	// (`true` allows public access) 
+	'*': true,
 
-  /*
-	// Here's an example of adding some policies to a controller
-	RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		'*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy 
-		// (this overrides `false` above)
-		nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		feed : ['isNiceToAnimals', 'hasRabbitFood']
+	MessagesController: {
+		create: 'MessageCreatePolicy'
 	}
-	*/
 };
 
+/*
+// Here's an example of adding some policies to a controller
+RabbitController: {
+	// Apply the `false` policy as the default for all of RabbitController's actions
+	// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
+	'*': false,
+
+	// For the action `nurture`, apply the 'isRabbitMother' policy 
+	// (this overrides `false` above)
+	nurture	: 'isRabbitMother',
+
+	// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
+	// before letting any users feed our rabbits
+	feed : ['isNiceToAnimals', 'hasRabbitFood']
+}
+*/
 
 /**
  * Here's what the `isNiceToAnimals` policy from above might look like:
